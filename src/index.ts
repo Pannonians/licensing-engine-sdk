f31a228d67180b89e8f624652fdb8282b489ef91
pvukmanovic-pannonians <petar.vukmanovic@pannonians.com>
2023-01-23 12:45:10 +0100

    improve jsdoc

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { GetLicensesApi } from './api/GetLicenses'
 import { createApi } from './http'
 dotenv.config()
 
+/**
+ * Represents an licensing engine SDK.
+ * @param {('domain'|'token')} authMethod - Method. Can be "domain" or "token".
+ * @param {string} token - Bearer token. Required if authMethod is "token".
+ * @returns {Promise} - Returns an async axios response
+ */
 export const licensingEngineApi = (authMethod: 'domain' | 'token', token = null) =>
   createApi<{
     getLicenses: GetLicensesApi
