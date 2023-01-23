84a9275d9b0b1940b32b12e998c990da76733ed5
pvukmanovic-pannonians <petar.vukmanovic@pannonians.com>
2023-01-23 12:32:44 +0100

    changes export name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { GetLicensesApi } from './api/GetLicenses'
 import { createApi } from './http'
 dotenv.config()
 
-export const api = (authMethod: 'domain' | 'token', token = null) =>
+export const licensingEngineApi = (authMethod: 'domain' | 'token', token = null) =>
   createApi<{
     getLicenses: GetLicensesApi
   }>(`${process.env.LICENSING_ENGINE_API_ORIGIN}`, authMethod, token)
